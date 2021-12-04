5cf4ace31ed3296ae027342b401e53240e3288d6
Rubus <Rubus2550327>
2021-12-04 17:57:03 +0800

    JdbcTemplate事务：propagation-事务传播行为

--- JdbcTemplate/src/main/java/com/transaction/service/UserService.java
+++ JdbcTemplate/src/main/java/com/transaction/service/UserService.java
@@ -3,10 +3,11 @@ package com.transaction.service;
 import com.transaction.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@Transactional(propagation = Propagation.REQUIRED)
 public class UserService {
 
     private final UserDao userDao;
