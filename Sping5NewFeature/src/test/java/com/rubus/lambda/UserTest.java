703b212fe38420e7928b8aea1c60ceec8747879d
Rubus <Rubus2550327>
2021-12-12 12:38:30 +0800

    Spring5函数式编程：注册使用自己创建的对象-使用别名的方式

--- Sping5NewFeature/src/test/java/com/rubus/lambda/UserTest.java
+++ Sping5NewFeature/src/test/java/com/rubus/lambda/UserTest.java
@@ -15,7 +15,20 @@ class UserTest {
         context.refresh();
         context.registerBean(User.class, () -> new User());
         //3. 获取在Spring注册的对象
-        User user = (User)context.getBean("com.rubus.lambda.User");
+        User user = (User) context.getBean("com.rubus.lambda.User");
+        System.out.println(user);
+    }
+
+    //函数式风格创建对象，交给spring进行管理
+    @Test
+    public void lambdaCreateObject1() {
+        //1. 创建GenericApplicationContext
+        GenericApplicationContext context = new GenericApplicationContext();
+        //2. 调用context的方法对象注册
+        context.refresh();
+        context.registerBean("user1", User.class, () -> new User());
+        //3. 获取在Spring注册的对象
+        User user = (User) context.getBean("user1");
         System.out.println(user);
     }
 
