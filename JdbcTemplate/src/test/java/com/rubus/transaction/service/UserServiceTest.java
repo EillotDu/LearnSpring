2f8cb382719744a8b76a90edec73bbe1ea4d80f4
Rubus <Rubus2550327>
2021-12-12 11:40:26 +0800

    JdbcTemplate事务：完全注解声明式事务管理

--- JdbcTemplate/src/test/java/com/rubus/transaction/service/UserServiceTest.java
+++ JdbcTemplate/src/test/java/com/rubus/transaction/service/UserServiceTest.java
@@ -1,8 +1,9 @@
-package com.transaction.service;
+package com.rubus.transaction.service;
 
-import org.junit.jupiter.api.BeforeEach;
+import com.rubus.transaction.config.TxConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
@@ -20,6 +21,15 @@ class UserServiceTest {
         ApplicationContext context = new ClassPathXmlApplicationContext("/xmlfortransactional.xml");
         UserService userService = context.getBean("userService", UserService.class);
         userService.accountMoney();
+    }
+
+    @Test
+    public void totalAnnotationTransaction() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(TxConfig.class);
+        UserService userService = context.getBean("userService", UserService.class);
+        userService.accountMoney();
+
+
     }
 
 }
