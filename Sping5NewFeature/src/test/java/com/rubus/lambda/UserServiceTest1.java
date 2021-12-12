1f817be1adb282cfaa0eb2c6d78b6d41086397a9
Rubus <Rubus2550327>
2021-12-12 13:24:48 +0800

    Spring5-test：整合JUnit5，旧的写法

--- Sping5NewFeature/src/test/java/com/rubus/lambda/UserServiceTest1.java
+++ Sping5NewFeature/src/test/java/com/rubus/lambda/UserServiceTest1.java
@@ -1,20 +1,21 @@
 package com.rubus.lambda;
 
 import com.rubus.service.UserService;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-/*
-Spring5-test 整合JUnit4
+/**
+ *
  */
-
-@RunWith(SpringJUnit4ClassRunner.class)
+/*
+Spring-test整合JUnit5
+ */
+@ExtendWith(SpringExtension.class)
 @ContextConfiguration("classpath:bean1.xml")
-public class UserServiceTest {
+public class UserServiceTest1 {
 
     @Autowired
     private UserService userService;
